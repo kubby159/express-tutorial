70cb75ad406c781a7ebc12eff5fe98bcd5648cf8
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-06 00:32:40 +0900

    [express] how to GET request

---
+++ server.js
@@ -0,0 +1,18 @@
+//express 불러오기와 기본셋팅
+const express = require("express");
+const app = express();
+
+app.listen(8080, function () {
+  console.log("Hello world");
+});
+
+/*작성 예시
+
+1.누군가가 /pet 으로 방문하면
+2.pet관련된 안내문을 띄워주자
+
+*/
+
+app.get("/pet", function (request, response) {
+  response.send("here is Pet Zone");
+});
