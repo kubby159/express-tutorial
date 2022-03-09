06b3f1530f90bbe7c11c4003550d89d0f3951526
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-03-09 22:15:50 +0900

    [Express] Test

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.listen(8080, function () {
   console.log("Hello world");
 });
 
+//get('경로', 화면을 띄운 후 실행할 함수.)
 app.get("/pet", function (request, response) {
   response.send("펫용품 쇼핑할 수 있는 사이트입니다.");
+});
+
+app.get("/beauty", function (request, response) {
+  response.send("뷰티용품 쇼핑 페이지입니다.");
 });
