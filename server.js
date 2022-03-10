6f7da72df62a4a6390e5fc67381f06264455f704
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-03-10 18:43:01 +0900

    [Express] i saved data in MongoDB

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let db;
 const MongoClient = require("mongodb").MongoClient;
 MongoClient.connect(
   "mongodb+srv://admin:qwer1234@cluster0.5bxyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   (error, client) => {
+    //디비 접속되면 할 일
+    if (error) {
+      return console.log(error);
+    }
+
+    //todoapp 이라는 db로 연결
+    // db = client.db("todoapp");
+
+    //post 라는 파일(collection) 에 insertOne 한다. 저장할 데이터에는 Object 자료형이 들어간다.
+    // db.collection('post').insertOne('저장할 데이터',(error,result)=>{
+    //   console.log('저장완료')
+    // });
+
     app.listen(8080, function () {
       console.log("디비접속성공");
     });
@@ -34,7 +48,17 @@ app.get("/write", function (request, response) {
 });
 
 app.post("/add", (request, response) => {
-  response.send("전송완료");
-  console.log(request.body.detail);
-  console.log(request.body.title);
+  MongoClient.connect(
+    "mongodb+srv://admin:qwer1234@cluster0.5bxyw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    (error, client) => {
+      db = client.db("todoapp");
+      db.collection("post").insertOne(
+        { title: request.body.title, detail: request.body.detail },
+        (error, result) => {
+          console.log(error);
+          console.log(result);
+        }
+      );
+    }
+  );
 });
